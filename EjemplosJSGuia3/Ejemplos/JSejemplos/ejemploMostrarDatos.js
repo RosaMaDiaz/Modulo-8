9236e78b9d042d20fdbdf4d3ce1345344cdf0ba1
Rosa Diaz <rosadiazitup@gmail.com>
2026-06-07 23:02:29 -0400

    Agregar archivos mostrar datos

---
+++ EjemplosJSGuia3/Ejemplos/JSejemplos/ejemploMostrarDatos.js
@@ -0,0 +1,11 @@
+let persona = {
+    nombre: "Juan",
+    edad: 30
+    
+};
+
+document.getElementById("nombre")
+    .textContent = persona.nombre;
+
+document.getElementById("edad")
+    .textContent = "Edad: " + persona.edad;
